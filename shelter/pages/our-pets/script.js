363b41e982ecf8c3a258cd53f425222df4f2a42f
Ivsmcrew <Ivsmcrew@gmail.com>
2023-07-09 18:50:41 +0300

    feat: implement the mobile menu for the second page

--- shelter/pages/our-pets/script.js
+++ shelter/pages/our-pets/script.js
@@ -1,1 +1,7 @@
-`use strict`
+import Menu from "../../assets/js/Menu.js"; 
+
+window.onload = function() {
+    let menu = new Menu();
+    menu.menuDeviceChangeHandler();
+    menu.menuHandler();
+}
